03ed07251c5b907e25353d4ece37a0ec7a247adb
vadimghedreutan <vadim.ghedreutan@icloud.com>
2025-08-28 02:16:29 +0200

    chore(csp): allow inline scripts in prod for Next boot

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const PROD_CSP = `
   img-src 'self' data: blob: https:;
   font-src 'self' data:;
   style-src 'self' 'unsafe-inline';
-  script-src 'self';
+  script-src 'self' 'unsafe-inline';
   connect-src 'self' https:;
   form-action 'self';
   upgrade-insecure-requests;
